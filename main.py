177dc042d2814da63b364cd582d74f2c7dd1114f
ViniciusSimm <vinao.simm@gmail.com>
2022-03-09 21:40:34 -0300

    load dataset

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import seaborn as sns
+import sklearn
+import pandas as pd
+from sklearn import datasets
+
+diabetes_X, diabetes_Y = datasets.load_diabetes(return_X_y=True)
+full_database = pd.DataFrame(diabetes_X,columns=['age','sex','bmi','bp','s1_tc','s2_ldl','s3_hdl','s4_tch','s5_ltg','s6_glu'])
+full_database['disease_progression'] = diabetes_Y
