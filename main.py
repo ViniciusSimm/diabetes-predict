9d1b58818c34fe53881b4ef37164f86e8e23968e
ViniciusSimm <vinao.simm@gmail.com>
2022-03-09 21:43:14 -0300

    add print database

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ from sklearn import datasets
 diabetes_X, diabetes_Y = datasets.load_diabetes(return_X_y=True)
 full_database = pd.DataFrame(diabetes_X,columns=['age','sex','bmi','bp','s1_tc','s2_ldl','s3_hdl','s4_tch','s5_ltg','s6_glu'])
 full_database['disease_progression'] = diabetes_Y
+
+print(full_database)
