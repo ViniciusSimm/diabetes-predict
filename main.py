0258a19c95b6b5e3b02533f2ac5cc4dfaf9b3123
ViniciusSimm <vinao.simm@gmail.com>
2022-03-09 22:13:19 -0300

    first graphs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sklearn
 import pandas as pd
 from sklearn import datasets
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 X, Y = datasets.load_diabetes(return_X_y=True)
 full_database = pd.DataFrame(X,columns=['age','sex','bmi','bp','s1_tc','s2_ldl','s3_hdl','s4_tch','s5_ltg','s6_glu'])
@@ -20,3 +22,6 @@ print("-"*100)
 print("Once all the other variables were quantitatives:")
 
 print(full_database.drop('sex',axis=1).describe())
+
+sns.displot(full_database, x="age")
+plt.show()
